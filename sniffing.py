09b3781887b88e2e5dcd2e9fa4af788e9dc86d72
Amirhosein Ataei <ataeiamirhosein@gmail.com>
2020-05-28 16:13:45 +0200

    Add files via upload

--- sniffing.py
+++ sniffing.py
@@ -1,4 +1,10 @@
-#author amirhosein ataei
+
+#<author amirhosein ataei
+#important github repo
+# thanks from https://github.com/statisticsniff/howmanypeoplearearound
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 import pandas as pd
 import time
 import os
@@ -20,7 +26,12 @@ from colorama import Fore, Back, Style
 from pick import pick
 from getpass import getpass
 from subprocess import Popen, PIPE
-#initialize the color library
+
+
+#initializing section
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 colorama.init()
 #set the specific background and foreground color to black and white for seprating reason 
 print(Fore.WHITE + Back.BLACK)
@@ -29,7 +40,7 @@ print(Fore.RED + '\n        |         |  _)      |  _)      \n    __| __|  _` | 
 print(Fore.MAGENTA + '          github.com/ataeiamirhosein\n------------------------------------------------------\n- performance depend your wireless card \n- connect internet and be sure support monitor mode ')
 #reset the colors to the black and white and continue
 print(Fore.WHITE + Back.BLACK)
-print("loading files ...\n")
+print("loading files take a moments connecting ...\n")
 #getting a oui file from the valid github repo that took from ieee source
 urlgit = 'https://raw.githubusercontent.com/statisticsniff/howmanypeoplearearound/master/oui.txt'
 #send a download request to the server and save in object req
@@ -38,7 +49,8 @@ req = requests.get(urlgit, allow_redirects=True)
 inp = input('> your password needed only using for root commands to continue.\n> be careful to enter inputs correct.\n\n* do you continue?\n[yes] continue, [no] stop\n' + Fore.RED + '-> ')
 if inp == 'yes':
     #reset the colors again and start the initializing processes
-    print(Fore.WHITE + Back.BLACK + "\n-->running process ...\n")
+    print(Fore.YELLOW + "\n-->running process ...\n")
+    print(Fore.WHITE + Back.BLACK)
     pass
 elif inp == 'no':
     #stop the process and close the program by choosing item no 
@@ -58,7 +70,12 @@ outze = procze.communicate(password.encode())
 #
 procon = Popen("sudo -S chmod 777 oui.txt".split(), stdin=PIPE, stdout=PIPE, stderr=PIPE)
 outon = procon.communicate(password.encode())
-#
+
+
+#reading vendors info section
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 fileon = open('oui.txt', 'wb')
 fileon.write(req.content)
 fileon.close()
@@ -78,10 +95,15 @@ for h in range (0, len(vendor_name)):
     tempo = vendor_name[h]
     tempo = tempo.replace('\n', '')
     vendor_name[h] = tempo
-#
+#fill other negligible vendor with unknown
 UNIQUE_VENDOR = numpy.unique(vendor_name)
 UNIQUE_VENDOR = numpy.append(UNIQUE_VENDOR, "UNKOWN")
-#
+
+
+#sniffing packet section
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 proctw = Popen("sudo -S touch result.html".split(), stdin=PIPE, stdout=PIPE, stderr=PIPE)
 outtw = proctw.communicate(password.encode())
 #
@@ -115,13 +137,20 @@ d = Popen("iwconfig", stdin=PIPE, stdout=PIPE, stderr=PIPE)
 od = d.communicate()
 print(od)
 #
-print("\n-->scaning frames ...")
+print(Fore.YELLOW)
+print("\n-->scaning frames take a moments ...")
+print(Fore.WHITE + Back.BLACK)
 #
-procsi = Popen(["tshark", "-i", option, "-I", "-a", "packets:256", "-w", "cap.pcapng", "-F", "pcapng"], stdout=PIPE, stderr=PIPE)
+procsi = Popen(["tshark", "-i", option, "-I", "-a", "packets:128", "-w", "cap.pcapng", "-F", "pcapng"], stdout=PIPE, stderr=PIPE)
 outsi = procsi.communicate()
 #
 cap = pyshark.FileCapture('cap.pcapng')
-#
+
+
+#filtering probes section
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 ssid_list = []
 mac_list = []
 rssi_list = []
@@ -149,8 +178,15 @@ for packet in cap:
     except:
         pass
 #finish the search of data from captured packets
+print(Fore.YELLOW)
 print("\n-->captured " + str(countero) + " probes")
-#
+print(Fore.WHITE + Back.BLACK)
+
+
+#exploiting crucial information section
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 unique_mac = numpy.unique(mac_list)
 unique_ssid = numpy.unique(ssid_list)
 #
@@ -183,10 +219,43 @@ for w in range (0, len(number)):
     names.append(vendor_name[number[w]])  
 #
 print(names)
+
+
+#indexing section
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+indexo = []
+indext = []
+#
+for l in range(0, len(unique_ssid)):
+    for k in range(0, len(ssid_list)):
+        if unique_ssid[l] == ssid_list[k]:
+            indexo.append(l)
+            indext.append(k)
+#
+# unique_indexo = numpy.unique(indexo)
+#
+print(unique_indexo)
+print(indext)
+#
+indexth = []
+#
+for n in range(0, len(find)):
+    for m in range(0, len(unique_mac)):
+        if find[n] == unique_mac[m]:
+            indexth.append(m)
+#
+print(indexth)
+
+
 #innovate a method for seprating list of devices that use by people getting from gsm source to another equipment like access points with filtering
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 devices = ['Motorola Mobility LLC, a Lenovo Company',
            'GUANGDONG OPPO MOBILE TELECOMMUNICATIONS CORP.,LTD',
-           'Huawei Symantec Technologies Co.,Ltd.',
+           'HUAWEI TECHNOLOGIES CO.,LTD',
            'Microsoft Corporation',
            'HTC Corporation',
            'Samsung Electronics Co.,Ltd',
@@ -217,20 +286,35 @@ if v == 0:
     print("> no active person around you")
 else:
     print('> about ' + Fore.RED + str(v) + Fore.BLUE + ' active person available near you!')
-#
+
+
+#time complexity section
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 temp_time = time.time()
 total_time = temp_time - start_time
-#time complexity
+#calculate tic toc time
 print(Fore.MAGENTA + '\n> time complexity= ' + str(total_time) + " sec")
 print(Fore.GREEN)
-#
-s = pd.Series([1,2,3])
+
+
+#plot section
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+s = pd.Series([1, 2, 3, 4])
 fig, ax = plt.subplots()
 s.plot.bar()
 fig.savefig('plot.png')
-#filling the statistic out file
+
+
+#statistic out file section
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ 
 filetw = open('result.html', 'w')
-filetw.write("""<!DOCTYPE html>\n<html>\n<head>\n<title>statistic_wifi_sniff</title>\n</head>\n<body>\n<div align='center'>\n<h2>statistic wifi sniff</h2>\n<hr>\n<img src='plot.png'>\n<p><font color='red'>-the closer it is to zero, the stronger the signal is. so it is nearest.</font></p>\n<br>\n<hr>\n<p>a total physical address near you = """ + str(unique_mac) + """ that probably active person = """ + str(find) + """</p>\n<p>the name of people = """ + str(unique_ssid) + """</p>\n<p>name of equipment vendor = """ + str(names) + """</p>\n<hr>\n<p>-see <a href="https://github.com/ataeiamirhosein/StatisticWifiSniff/blob/master/devices.txt" target="_blank">LIST</a> of sorted vendor that produce equipment that used by people such as phone, tablet and laptop.</p><hr>\n<p><a href="https://github.com/ataeiamirhosein" target="_blank">amirhosein ataei</a><p>\n</div>\n</body>\n</html>""")
+filetw.write("""<!DOCTYPE html>\n<html>\n<head>\n<title>statistic_wifi_sniff</title>\n</head>\n<body>\n<div align='center'>\n<h2>statistic wifi sniff</h2>\n<hr>\n<img src='plot.png'>\n<p><font color='green'>-the closer it is to zero, the stronger the signal is. </font><font color='purple'>so it is nearest.</font></p>\n<br>\n<hr>\n<p><font color='blue'>> total physical address near you = </font>""" + str(unique_mac) + """<font color='red'> that probably active person = </font>""" + str(find) + """</p>\n<p><font color='brown'>> the name of people = </font>""" + str(unique_ssid) + """</p>\n<p><font color='brown'>> name of equipment vendor = </font>""" + str(names) + """</p>\n<hr>\n<p>-see <a href="https://github.com/ataeiamirhosein/StatisticWifiSniff/blob/master/devices.txt" target="_blank">LIST</a> of sorted vendor that produce equipment that used by people such as phone, tablet and laptop.</p><hr>\n<p><a href="https://github.com/ataeiamirhosein" target="_blank">amirhosein ataei</a><p>\n</div>\n</body>\n</html>""")
 filetw.close()
 #now we can see all the data in result file for devices and in cap file about capture frame also oui file
 print('\n* see all detection in result file that will be open automatically\n\n* all operations was succesfull so stop *\n' + Style.RESET_ALL)
@@ -243,4 +327,9 @@ ofinal = final.communicate(password.encode())
 #open the out file via browser automatically
 urlhtml = 'result.html'
 webbrowser.open(urlhtml, new=2)
-#end of program
+
+
+#end of program>
+#reference
+# thanks from https://macaddress.io/
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
